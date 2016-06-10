113505723ec03a74b66e2106968125b9ad7f4949
ZeroNoRyouki <zeronoryouki@gmail.com>
2016-06-10 14:42:02 +0200

    Commented out IIcon stuff Commented stuff temporarily to allow this thing to compile...

--- src/main/java/erogenousbeef/bigreactors/gui/controls/GuiIconButton.java
+++ src/main/java/erogenousbeef/bigreactors/gui/controls/GuiIconButton.java
@@ -3,37 +3,41 @@ package erogenousbeef.bigreactors.gui.controls;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.util.IIcon;
-
 import org.lwjgl.opengl.GL11;
-
 import erogenousbeef.bigreactors.gui.IBeefTooltipControl;
 
 public class GuiIconButton extends GuiButton implements IBeefTooltipControl {
 
-	protected IIcon icon;
+	//TODO Commented out IIcon stuff
+	//protected IIcon icon;
 	
 	protected String[] tooltip;
-	
+
+
 	public GuiIconButton(int buttonId, int x, int y, int width, int height) {
 		super(buttonId, x, y, width, height, "");
-		icon = null;
+		//TODO Commented out IIcon stuff
+		//icon = null;
 		tooltip = null;
 	}
 
-	public GuiIconButton(int buttonId, int x, int y, int width, int height, IIcon icon) {
+	//TODO Commented out IIcon stuff
+	public GuiIconButton(int buttonId, int x, int y, int width, int height, /*IIcon*/Object icon) {
 		this(buttonId, x, y, width, height);
-		this.icon = icon;
+		//TODO Commented out IIcon stuff
+		//this.icon = icon;
 		tooltip = null;
 	}
 
-	public GuiIconButton(int buttonId, int x, int y, int width, int height, IIcon icon, String[] tooltip) {
+	//TODO Commented out IIcon stuff
+	public GuiIconButton(int buttonId, int x, int y, int width, int height, /*IIcon*/Object icon, String[] tooltip) {
 		this(buttonId, x, y, width, height, icon);
 		this.tooltip = tooltip;
 	}
-	
-	public void setIcon(IIcon icon) {
-		this.icon = icon;
+
+	//TODO Commented out IIcon stuff
+	public void setIcon(/*IIcon*/Object icon) {
+		//this.icon = icon;
 	}
 
     /**
@@ -46,19 +50,25 @@ public class GuiIconButton extends GuiButton implements IBeefTooltipControl {
             GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 
             // Draw the border
+			// TODO Commented temporarily to allow this thing to compile...
+			/*
             this.field_146123_n = par2 >= this.xPosition && par3 >= this.yPosition && par2 < this.xPosition + this.width && par3 < this.yPosition + this.height;
             int k = this.getHoverState(this.field_146123_n);
+            */int k = 0;
             int borderColor = k == 2 ? 0xFF5555AA : 0xFF000000;
         	drawRect(this.xPosition, this.yPosition, this.xPosition + this.width, this.yPosition + this.height, borderColor);
             
             this.mouseDragged(par1Minecraft, par2, par3);
 
             // Draw the icon
+			//TODO Commented out IIcon stuff
+			/*
             if(this.icon != null) {
                 GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
                 par1Minecraft.renderEngine.bindTexture(TextureMap.locationBlocksTexture);
             	this.drawTexturedModelRectFromIcon(this.xPosition + 1, this.yPosition + 1, this.icon, this.width-2, this.height-2);
             }
+            */
         }
     }
 
