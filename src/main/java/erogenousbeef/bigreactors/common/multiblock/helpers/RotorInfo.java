5590149120c0a5b20b1f32926a6f0aec0856c282
ZeroNoRyouki <zeronoryouki@gmail.com>
2016-06-10 13:13:47 +0200

    Conversion to BlockPos

--- src/main/java/erogenousbeef/bigreactors/common/multiblock/helpers/RotorInfo.java
+++ src/main/java/erogenousbeef/bigreactors/common/multiblock/helpers/RotorInfo.java
@@ -1,13 +1,13 @@
 package erogenousbeef.bigreactors.common.multiblock.helpers;
 
-import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraft.util.EnumFacing;
 
 public class RotorInfo {
 	// Location of bearing
 	public int x, y, z;
 	
 	// Rotor direction
-	public ForgeDirection rotorDirection = ForgeDirection.UNKNOWN;
+	public EnumFacing rotorDirection;
 	
 	// Rotor length
 	public int rotorLength = 0;
