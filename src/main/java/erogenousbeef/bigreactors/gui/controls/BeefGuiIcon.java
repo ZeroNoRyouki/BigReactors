63f2502ff3aa2a27c34361115d119b31c61e5a6f
ZeroNoRyouki <zeronoryouki@gmail.com>
2016-06-10 14:38:24 +0200

    Commented out IIcon stuff

--- src/main/java/erogenousbeef/bigreactors/gui/controls/BeefGuiIcon.java
+++ src/main/java/erogenousbeef/bigreactors/gui/controls/BeefGuiIcon.java
@@ -2,26 +2,26 @@ package erogenousbeef.bigreactors.gui.controls;
 
 import net.minecraft.client.renderer.texture.TextureManager;
 import net.minecraft.client.renderer.texture.TextureMap;
-import net.minecraft.util.IIcon;
-
 import org.lwjgl.opengl.GL11;
-
 import erogenousbeef.bigreactors.client.gui.BeefGuiBase;
 import erogenousbeef.bigreactors.gui.BeefGuiControlBase;
 import erogenousbeef.bigreactors.gui.IBeefTooltipControl;
 
 public class BeefGuiIcon extends BeefGuiControlBase implements IBeefTooltipControl {
 
-	protected IIcon icon;
+	// TODO Commented out IIcon stuff
+	//protected IIcon icon;
 	protected String[] tooltip;
 
 	public BeefGuiIcon(BeefGuiBase container, int absoluteX, int absoluteY) {
 		this(container, absoluteX, absoluteY, 16, 16);
 	}
-	
-	public BeefGuiIcon(BeefGuiBase container, int absoluteX, int absoluteY, int sizeX, int sizeY, IIcon icon, String[] tooltip) {
+
+	// TODO Commented out IIcon stuff
+	public BeefGuiIcon(BeefGuiBase container, int absoluteX, int absoluteY, int sizeX, int sizeY, /*IIcon*/Object icon, String[] tooltip) {
 		this(container, absoluteX, absoluteY, sizeX, sizeY);
-		this.icon = icon;
+		// TODO Commented out IIcon stuff
+		//this.icon = icon;
 		this.tooltip = tooltip;
 	}
 
@@ -29,24 +29,31 @@ public class BeefGuiIcon extends BeefGuiControlBase implements IBeefTooltipContr
 	public BeefGuiIcon(BeefGuiBase container, int absoluteX, int absoluteY,
 			int width, int height) {
 		super(container, absoluteX, absoluteY, width, height);
-		icon = null;
+		// TODO Commented out IIcon stuff
+		//icon = null;
 		tooltip = null;
 	}
 
-	public void setIcon(IIcon icon) {
-		this.icon = icon;
+	// TODO Commented out IIcon stuff
+	public void setIcon(/*IIcon*/Object icon) {
+		//this.icon = icon;
 	}
 	
 	@Override
 	public void drawBackground(TextureManager renderEngine, int mouseX,
 			int mouseY) {
-		if(!visible) { return; }
+		if(!visible) { return;
+		}
+
+		// TODO Commented out IIcon stuff
+		/*
 		if(icon == null) { return; }
 		
 
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         renderEngine.bindTexture(TextureMap.locationBlocksTexture);
     	drawTexturedModelRectFromIcon(this.absoluteX, this.absoluteY, this.icon, this.width, this.height);
+    	*/
 	}
 
 	@Override
