591260b2d2ea31206f57334dc629b5b89a024f7c
ZeroNoRyouki <zeronoryouki@gmail.com>
2016-06-10 12:55:38 +0200

    Commented out IIcon stuff Updated to new Vanilla methods signature Commented stuff temporarily to allow this thing to compile...

--- src/main/java/erogenousbeef/bigreactors/common/item/ItemBRBucket.java
+++ src/main/java/erogenousbeef/bigreactors/common/item/ItemBRBucket.java
@@ -3,11 +3,13 @@ package erogenousbeef.bigreactors.common.item;
 import java.util.List;
 
 import net.minecraft.block.Block;
-import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBucket;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -23,22 +25,32 @@ public class ItemBRBucket extends ItemBucket {
 		_fluid = fluid;
 	}
 
+	// TODO Commented out IIcon stuff
+	/*
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void registerIcons(IIconRegister iconRegistry) {
 		this.itemIcon = iconRegistry.registerIcon(BigReactors.TEXTURE_NAME_PREFIX + getUnlocalizedName());
 	}
+	*/
 
 	@Override
-	public boolean tryPlaceContainedLiquid(World world, int x, int y, int z) {
+	public boolean tryPlaceContainedLiquid(EntityPlayer player, World world, BlockPos position) {
 		if(_fluid == null) {
 			return false;
 		}
-		else if(!world.isAirBlock(x, y, z) && world.getBlock(x, y, z).getMaterial().isSolid()) {
+
+		IBlockState state = world.getBlockState(position);
+		Block block = state.getBlock();
+
+		if(!world.isAirBlock(position) && block.getMaterial(state).isSolid()) {
 			return false;
 		}
 		else {
+			// TODO Commented temporarily to allow this thing to compile...
+			/*
 			world.setBlock(x, y, z, _fluid, 0, 3);
+			*/
 			return true;
 		}
 	}
