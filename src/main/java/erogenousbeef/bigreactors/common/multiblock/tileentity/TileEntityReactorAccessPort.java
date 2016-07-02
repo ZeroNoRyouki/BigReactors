7ea53d804fdda5f09e4f36f5f8920a12f527bf43
ZeroNoRyouki <zeronoryouki@gmail.com>
2016-07-02 13:40:41 +0200

    Replaced IInventory with IItemHandler capability

--- src/main/java/erogenousbeef/bigreactors/common/multiblock/tileentity/TileEntityReactorAccessPort.java
+++ src/main/java/erogenousbeef/bigreactors/common/multiblock/tileentity/TileEntityReactorAccessPort.java
@@ -7,18 +7,13 @@ import erogenousbeef.bigreactors.init.BrItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-//import cofh.lib.util.helpers.BlockHelper;
-//import cofh.lib.util.helpers.ItemHelper;
 import erogenousbeef.bigreactors.api.data.SourceProductMapping;
 import erogenousbeef.bigreactors.api.registry.Reactants;
 import erogenousbeef.bigreactors.client.gui.GuiReactorAccessPort;
@@ -26,47 +21,66 @@ import erogenousbeef.bigreactors.common.BRLog;
 import erogenousbeef.bigreactors.common.data.StandardReactants;
 import erogenousbeef.bigreactors.common.multiblock.interfaces.INeighborUpdatableEntity;
 import erogenousbeef.bigreactors.gui.container.ContainerReactorAccessPort;
-import erogenousbeef.bigreactors.utils.AdjacentInventoryHelper;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.ItemStackHandler;
 import zero.mods.zerocore.api.multiblock.MultiblockControllerBase;
 import zero.mods.zerocore.lib.item.TileEntityItemStackHandler;
+import zero.mods.zerocore.util.OreDictionaryHelper;
 import zero.mods.zerocore.util.WorldHelper;
 
 public class TileEntityReactorAccessPort extends TileEntityReactorPart implements INeighborUpdatableEntity {
 
-	protected TileEntityItemStackHandler _inventories;
-
-	protected boolean isInlet;
-	protected AdjacentInventoryHelper adjacencyHelper;
-	
-	public static final int SLOT_INLET = 0;
-	public static final int SLOT_OUTLET = 1;
-	
-	private static final int[] kInletExposed = {SLOT_INLET};
-	private static final int[] kOutletExposed = {SLOT_OUTLET};
-	
 	public TileEntityReactorAccessPort() {
 
-		super();
-		this._inventories = new TileEntityItemStackHandler(this, 2);
-		isInlet = true;
+		this._isInlet = true;
+		this._adjacentInventory = null;
+		this._fuelInventoryWrapper = this._wasteInventoryWrapper = null;
+		this._fuelInventory = new TileEntityItemStackHandler(this, 1);
+		this._wasteInventory = new TileEntityItemStackHandler(this, 1);
+	}
+
+	public ItemStackHandler getItemStackHandler(boolean fuel) {
+		return fuel ? this._fuelInventory : this._wasteInventory;
 	}
 
 	@Override
 	public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
-
-		return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY == capability ?
-			true : super.hasCapability(capability, facing);
+		return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY == capability || super.hasCapability(capability, facing);
 	}
 
 	@Override
 	public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
 
-		if (CapabilityItemHandler.ITEM_HANDLER_CAPABILITY == capability)
-			return (T)this._inventories;
+		if (CapabilityItemHandler.ITEM_HANDLER_CAPABILITY == capability) {
+
+			if (this._isInlet) {
+
+				if (null == this._fuelInventoryWrapper)
+					this._fuelInventoryWrapper = new CapabWrapper(this._fuelInventory) {
+						@Override
+						public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
+							return null != stack && Reactants.isFuel(stack) ? this._handler.insertItem(slot, stack, simulate) : stack;
+						}
+					};
+
+				return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.cast(this._fuelInventoryWrapper);
+
+			} else {
+
+				if (null == this._wasteInventoryWrapper)
+					this._wasteInventoryWrapper = new CapabWrapper(this._wasteInventory) {
+						@Override
+						public ItemStack insertItem(int slot, ItemStack stack, boolean simulate) {
+							return stack;
+						}
+					};
+
+				return CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.cast(this._wasteInventoryWrapper);
+			}
+		}
 
 		return super.getCapability(capability, facing);
 	}
@@ -76,21 +90,26 @@ public class TileEntityReactorAccessPort extends TileEntityReactorPart implement
 		return true;
 	}
 
-	// Return the name of the reactant to which the item in the input slot
+	/**
+	 * Return the name of the reactant to which the item in the input slot
+	 */
 	public String getInputReactantType() {
-		ItemStack inputItem = this._inventories.getStackInSlot(SLOT_INLET);
-		if(inputItem == null) { return null; }
-		SourceProductMapping mapping = Reactants.getSolidToReactant(inputItem);
-		return mapping != null ? mapping.getProduct() : null;
+
+		ItemStack inputItem = this._fuelInventory.getStackInSlot(0);
+		SourceProductMapping mapping = null != inputItem ? Reactants.getSolidToReactant(inputItem) : null;
+
+		return null != mapping ? mapping.getProduct() : null;
 	}
 
-	// Returns the potential amount of reactant which can be produced from this port.
+	/**
+	 * Returns the potential amount of reactant which can be produced from this port.
+     */
 	public int getInputReactantAmount() {
-		ItemStack inputItem = this._inventories.getStackInSlot(SLOT_INLET);
-		if(inputItem == null) { return 0; }
 
-		SourceProductMapping mapping = Reactants.getSolidToReactant(inputItem);
-		return mapping != null ? mapping.getProductAmount(inputItem.stackSize) : 0;
+		ItemStack inputItem = this._fuelInventory.getStackInSlot(0);
+		SourceProductMapping mapping = null != inputItem ? Reactants.getSolidToReactant(inputItem) : null;
+
+		return null != mapping ? mapping.getProductAmount(inputItem.stackSize) : 0;
 	}
 
 	/**
@@ -100,17 +119,15 @@ public class TileEntityReactorAccessPort extends TileEntityReactorPart implement
 	 * @return The amount of reactant actually produced, in reactant units (mB)
 	 */
 	public int consumeReactantItem(int reactantDesired) {
-		ItemStack inputItem = this._inventories.getStackInSlot(SLOT_INLET);
-		if(inputItem == null) { return 0; }
-		
-		SourceProductMapping mapping = Reactants.getSolidToReactant(inputItem);
-		if(mapping == null) { return 0; }
-		
-		int sourceItemsToConsume = Math.min(inputItem.stackSize, mapping.getSourceAmount(reactantDesired));
-		
-		if(sourceItemsToConsume <= 0) { return 0; }
 
-		this._inventories.extractItem(SLOT_INLET, sourceItemsToConsume, false);
+		ItemStack inputItem = this._fuelInventory.getStackInSlot(0);
+		SourceProductMapping mapping = null != inputItem ? Reactants.getSolidToReactant(inputItem) : null;
+		int sourceItemsToConsume = null != mapping ? Math.min(inputItem.stackSize, mapping.getSourceAmount(reactantDesired)) : 0;
+		
+		if (sourceItemsToConsume <= 0)
+			return 0;
+
+		this._fuelInventory.extractItem(0, sourceItemsToConsume, false);
 
 		return mapping.getProductAmount(sourceItemsToConsume);
 	}
@@ -126,7 +143,7 @@ public class TileEntityReactorAccessPort extends TileEntityReactorPart implement
 	public int emitReactant(String reactantType, int amount) {
 		if(reactantType == null || amount <= 0) { return 0; }
 		
-		ItemStack outputItem = this._inventories.getStackInSlot(SLOT_OUTLET);
+		ItemStack outputItem = this._wasteInventory.getStackInSlot(0);
 		int outputItemMaxSize = null != outputItem ? outputItem.getMaxStackSize() : 64;
 
 		if (outputItem != null && outputItem.stackSize >= /*getInventoryStackLimit()*/outputItemMaxSize) {
@@ -196,8 +213,7 @@ public class TileEntityReactorAccessPort extends TileEntityReactorPart implement
 		int reactantConsumed = bestMapping.getSourceAmount(itemsToProduce);
 		itemsToProduce = bestMapping.getProductAmount(reactantConsumed);
 
-		// TODO Commented temporarily to allow this thing to compile...
-		ItemStack newItem = null;//ItemHelper.getOre(bestMapping.getProduct());
+		ItemStack newItem = OreDictionaryHelper.getOre(bestMapping.getProduct());
 		if(newItem == null) {
 			BRLog.warning("Could not find item for oredict entry %s, using cyanite instead.", bestMapping.getSource());
 			newItem = BrItems.ingotMetals.createItemStack(MetalType.Cyanite, 1);
@@ -207,8 +223,7 @@ public class TileEntityReactorAccessPort extends TileEntityReactorPart implement
 		}
 		
 		newItem.stackSize = itemsToProduce;
-		//setInventorySlotContents(SLOT_OUTLET, newItem);
-		this._inventories.setStackInSlot(SLOT_OUTLET, newItem);
+		this._wasteInventory.setStackInSlot(0, newItem);
 		onItemsReceived();
 		
 		return reactantConsumed;
@@ -217,19 +232,17 @@ public class TileEntityReactorAccessPort extends TileEntityReactorPart implement
 	// Multiblock overrides
 	@Override
 	public void onMachineAssembled(MultiblockControllerBase controller) {
-		super.onMachineAssembled(controller);
 
-		// TODO Commented temporarily to allow this thing to compile...
-		/*
-		adjacencyHelper = new AdjacentInventoryHelper(this.getOutwardsDir());
-		checkForAdjacentInventories();
-		*/
+		super.onMachineAssembled(controller);
+		this._adjacentInventory = null;
+		this.checkForAdjacentInventories();
 	}
 	
 	@Override
 	public void onMachineBroken() {
+
 		super.onMachineBroken();
-		adjacencyHelper = null;
+		this._adjacentInventory = null;
 	}
 
 	// TileEntity overrides
@@ -281,27 +294,31 @@ public class TileEntityReactorAccessPort extends TileEntityReactorPart implement
 	protected void saveToNBT(NBTTagCompound data) {
 
 		super.saveToNBT(data);
-		data.setBoolean("isInlet", this.isInlet);
-		data.setTag("inv", this._inventories.serializeNBT());
+		data.setBoolean("isInlet", this._isInlet);
+		data.setTag("invI", this._fuelInventory.serializeNBT());
+		data.setTag("invO", this._wasteInventory.serializeNBT());
 	}
 
 	@Override
 	protected void loadFromNBT(NBTTagCompound data) {
 
 		super.loadFromNBT(data);
+		this._isInlet = data.hasKey("isInlet") ? data.getBoolean("isInlet") : true;
 
-		this.isInlet = data.hasKey("isInlet") ? data.getBoolean("isInlet") : true;
+		if (data.hasKey("invI"))
+			this._fuelInventory.deserializeNBT((NBTTagCompound)data.getTag("invI"));
 
-		if (data.hasKey("inv"))
-			this._inventories.deserializeNBT((NBTTagCompound)data.getTag("inv"));
+		if (data.hasKey("invO"))
+			this._wasteInventory.deserializeNBT((NBTTagCompound)data.getTag("invO"));
 	}
 
+	// TODO FIX both
 	// MultiblockTileEntityBase
 	@Override
 	protected void encodeDescriptionPacket(NBTTagCompound packetData) {
 		super.encodeDescriptionPacket(packetData);
 		
-		packetData.setBoolean("inlet", isInlet);
+		packetData.setBoolean("inlet", this._isInlet);
 	}
 	
 	@Override
@@ -312,211 +329,6 @@ public class TileEntityReactorAccessPort extends TileEntityReactorPart implement
 			setInlet(packetData.getBoolean("inlet"));
 		}
 	}
-	
-	// IInventory
-
-	// TODO fake imp!
-	/*
-	@Override
-	public void closeInventory(EntityPlayer player) {
-
-	}
-
-	@Override
-	public int[] getSlotsForFace(EnumFacing side) {
-		return new int[0];
-	}
-
-	@Override
-	public boolean canInsertItem(int index, ItemStack itemStackIn, EnumFacing direction) {
-		return false;
-	}
-
-	@Override
-	public boolean canExtractItem(int index, ItemStack stack, EnumFacing direction) {
-		return false;
-	}
-
-	@Override
-	public ItemStack removeStackFromSlot(int index) {
-		return null;
-	}
-
-	@Override
-	public void openInventory(EntityPlayer player) {
-
-	}
-
-	@Override
-	public int getField(int id) {
-		return 0;
-	}
-
-	@Override
-	public void setField(int id, int value) {
-
-	}
-
-	@Override
-	public int getFieldCount() {
-		return 0;
-	}
-
-	@Override
-	public void clear() {
-
-	}
-
-	@Override
-	public String getName() {
-		return null;
-	}
-
-	@Override
-	public boolean hasCustomName() {
-		return false;
-	}
-
-	@Override
-	public ITextComponent getDisplayName() {
-		return null;
-	}
-	// end fake imp!
-
-
-
-	@Override
-	public int getSizeInventory() {
-		return NUM_SLOTS;
-	}
-
-	@Override
-	public ItemStack getStackInSlot(int slot) {
-		return _inventories[slot];
-	}
-
-	@Override
-	public ItemStack decrStackSize(int slot, int amount) {
-		if(_inventories[slot] != null)
-		{
-			if(_inventories[slot].stackSize <= amount)
-			{
-				ItemStack itemstack = _inventories[slot];
-				_inventories[slot] = null;
-				markDirty();
-				return itemstack;
-			}
-			ItemStack newStack = _inventories[slot].splitStack(amount);
-			if(_inventories[slot].stackSize == 0)
-			{
-				_inventories[slot] = null;
-			}
-
-            markDirty();
-			return newStack;
-		}
-		else
-		{
-			return null;
-		}
-	}
-
-	// TODO Commented temporarily to allow this thing to compile...
-
-	@Override
-	public ItemStack getStackInSlotOnClosing(int slot) {
-		return null;
-	}
-
-
-	@Override
-	public void setInventorySlotContents(int slot, ItemStack itemstack) {
-		_inventories[slot] = itemstack;
-		if(itemstack != null && itemstack.stackSize > getInventoryStackLimit())
-		{
-			itemstack.stackSize = getInventoryStackLimit();
-		}
-
-        markDirty();
-	}
-	*/
-
-	// TODO Commented temporarily to allow this thing to compile...
-	/*
-	@Override
-	public String getInventoryName() {
-		return "Access Port";
-	}
-
-	@Override
-	public boolean hasCustomInventoryName() {
-		return false;
-	}
-
-	@Override
-	public int getInventoryStackLimit() {
-		return 64;
-	}
-
-	@Override
-	public boolean isUseableByPlayer(EntityPlayer entityplayer) {
-		BlockPos position = this.getPos();
-		if(worldObj.getTileEntity(position) != this)
-		{
-			return false;
-		}
-		return entityplayer.getDistanceSq((double)position.getX() + 0.5D, (double)position.getY() + 0.5D, (double)position.getZ() + 0.5D) <= 64D;
-	}
-
-	// TODO Commented temporarily to allow this thing to compile...
-
-	@Override
-	public void openInventory() {
-	}
-
-	@Override
-	public void closeInventory() {
-	}
-
-	@Override
-	public boolean isItemValidForSlot(int slot, ItemStack itemstack) {
-		if(itemstack == null) { return true; }
-
-		if(slot == SLOT_INLET) {
-			return Reactants.isFuel(itemstack);
-		}
-		else if(slot == SLOT_OUTLET) {
-			return Reactants.isWaste(itemstack);
-		}
-		else {
-			return false;
-		}
-	}
-	*/
-
-	// ISidedInventory
-	// TODO Commented temporarily to allow this thing to compile...
-	/*
-	@Override
-	public int[] getAccessibleSlotsFromSide(int side) {
-		if(isInlet()) {
-			return kInletExposed;
-		}
-		else {
-			return kOutletExposed;
-		}
-	}
-
-	@Override
-	public boolean canInsertItem(int slot, ItemStack itemstack, int side) {
-		return isItemValidForSlot(slot, itemstack);
-	}
-
-	@Override
-	public boolean canExtractItem(int slot, ItemStack itemstack, int side) {
-		return isItemValidForSlot(slot, itemstack);
-	}
-	*/
 
 	@Override
 	public Object getServerGuiElement(int guiId, EntityPlayer player) {
@@ -532,20 +344,24 @@ public class TileEntityReactorAccessPort extends TileEntityReactorPart implement
 	 * Called when stuff has been placed in the access port
 	 */
 	public void onItemsReceived() {
+
 		distributeItems();
 		markChunkDirty();
 	}
 
-	public boolean isInlet() { return this.isInlet; }
+	public boolean isInlet() { return this._isInlet; }
 
 	public void setInlet(boolean shouldBeInlet) {
-		if(isInlet == shouldBeInlet) { return; }
 
-		isInlet = shouldBeInlet;
+		if (this._isInlet == shouldBeInlet)
+			return;
 
-		WorldHelper.notifyBlockUpdate(worldObj, this.getPos(), null, null);
+		this._isInlet = shouldBeInlet;
 
-		if(!worldObj.isRemote) {
+		WorldHelper.notifyBlockUpdate(this.worldObj, this.getPos(), null, null);
+
+		if (!this.worldObj.isRemote) {
+
 			distributeItems();
 			markChunkDirty();
 		}
@@ -554,37 +370,40 @@ public class TileEntityReactorAccessPort extends TileEntityReactorPart implement
 	}
 
 	public void toggleInlet() {
-
-		this.setInlet(!this.isInlet);
+		this.setInlet(!this._isInlet);
 	}
 	
 	protected void distributeItems() {
-		if(worldObj.isRemote) { return; }
-		if(adjacencyHelper == null) { return; }
-		
-		if(this.isInlet()) { return; }
-		
-		//_inventories[SLOT_OUTLET] = adjacencyHelper.distribute(_inventories[SLOT_OUTLET]);
-		this._inventories.setStackInSlot(SLOT_OUTLET, adjacencyHelper.distribute(this._inventories.getStackInSlot(SLOT_OUTLET)));
+
+		if (worldObj.isRemote || this._adjacentInventory == null || this.isInlet())
+			return;
+
+		this._wasteInventory.setStackInSlot(0, ItemHandlerHelper.insertItem(this._adjacentInventory,
+				this._wasteInventory.getStackInSlot(0), false));
 		this.markChunkDirty();
 	}
 	
 	protected void checkForAdjacentInventories() {
-		// TODO Commented temporarily to allow this thing to compile...
-		/*
-		ForgeDirection outDir = getOutwardsDir();
 
-		if(adjacencyHelper == null && outDir != ForgeDirection.UNKNOWN) {
-			adjacencyHelper = new AdjacentInventoryHelper(outDir);
-		}
+		EnumFacing facing = this.getSingleOutwardsDir();
+		IItemHandler candidateInventory = null;
 
-		if(adjacencyHelper != null && outDir != ForgeDirection.UNKNOWN) {
-			TileEntity te = worldObj.getTileEntity(xCoord + outDir.offsetX, yCoord + outDir.offsetY, zCoord + outDir.offsetZ);
-			if(adjacencyHelper.set(te)) {
-				distributeItems();
-			}
+		if (null == facing)
+			// this should never happen
+			throw new IllegalStateException("Reactor access port report multiple outward facing!");
+
+		TileEntity te = this.worldObj.getTileEntity(this.getWorldPosition().offset(facing));
+
+		if (null != te && te.hasCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, facing.getOpposite()))
+			candidateInventory = te.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, facing.getOpposite());
+		
+		if (this._adjacentInventory != candidateInventory) {
+
+			this._adjacentInventory = candidateInventory;
+
+			if (null != this._adjacentInventory)
+				this.distributeItems();
 		}
-		*/
 	}
 
 	// INeighborUpdateableEntity
@@ -595,12 +414,42 @@ public class TileEntityReactorAccessPort extends TileEntityReactorPart implement
 
 	@Override
 	public void onNeighborTileChange(IBlockAccess world, BlockPos position, BlockPos neighbor) {
-		// TODO Commented temporarily to allow this thing to compile...
-		/*
-		int side = BlockHelper.determineAdjacentSide(this, neighborX, neighborY, neighborZ);
-		if(side == getOutwardsDir().ordinal()) {
-			checkForAdjacentInventories();
+
+		EnumFacing facing = this.getSingleOutwardsDir();
+
+		// is the changed block the one we are facing?
+		if (null != facing && neighbor.equals(position.offset(facing)))
+			this.checkForAdjacentInventories();
+	}
+
+	protected TileEntityItemStackHandler _fuelInventory;
+	protected TileEntityItemStackHandler _wasteInventory;
+	protected IItemHandler _fuelInventoryWrapper;
+	protected IItemHandler _wasteInventoryWrapper;
+	protected IItemHandler _adjacentInventory;
+	protected boolean _isInlet;
+
+	private abstract class CapabWrapper implements IItemHandler {
+
+		public CapabWrapper(TileEntityItemStackHandler handler) {
+			this._handler = handler;
 		}
-		*/
+
+		@Override
+		public int getSlots() {
+			return this._handler.getSlots();
+		}
+
+		@Override
+		public ItemStack getStackInSlot(int slot) {
+			return this._handler.getStackInSlot(slot);
+		}
+
+		@Override
+		public ItemStack extractItem(int slot, int amount, boolean simulate) {
+			return this._handler.extractItem(slot, amount, simulate);
+		}
+
+		protected TileEntityItemStackHandler _handler;
 	}
 }
--- src/main/java/erogenousbeef/bigreactors/gui/slot/SlotReactorFuel.java
+++ src/main/java/erogenousbeef/bigreactors/gui/slot/SlotReactorFuel.java
@@ -1,17 +1,15 @@
 package erogenousbeef.bigreactors.gui.slot;
 
-import net.minecraft.inventory.IInventory;
-import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import erogenousbeef.bigreactors.api.registry.Reactants;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
 
-public class SlotReactorInput extends /*Slot*/SlotItemHandler {
+public class SlotReactorFuel extends SlotItemHandler {
 
 	boolean fuel = true;
 	
-	public SlotReactorInput(IItemHandler itemHandler, int index, int xPosition, int yPosition, boolean fuel) {
+	public SlotReactorFuel(IItemHandler itemHandler, int index, int xPosition, int yPosition, boolean fuel) {
 
 		super(itemHandler, index, xPosition, yPosition);
 		this.fuel = fuel;
