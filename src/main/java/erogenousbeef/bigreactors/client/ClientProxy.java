646f2fbd58ccddb2755bb2f6ff9c7349b71e99a0
ZeroNoRyouki <zeronoryouki@gmail.com>
2016-07-13 01:22:53 +0200

    Delegated model registration to blocks and items Added registration helper for fluids Added TESRs registration

--- src/main/java/erogenousbeef/bigreactors/client/ClientProxy.java
+++ src/main/java/erogenousbeef/bigreactors/client/ClientProxy.java
@@ -1,17 +1,30 @@
 package erogenousbeef.bigreactors.client;
 
+import erogenousbeef.bigreactors.client.renderer.RendererReactorFuelRod;
+import erogenousbeef.bigreactors.common.Properties;
 import erogenousbeef.bigreactors.common.block.BlockBR;
+import erogenousbeef.bigreactors.common.block.BlockBRGenericFluid;
 import erogenousbeef.bigreactors.common.item.ItemBase;
 import erogenousbeef.bigreactors.common.multiblock.MultiblockTurbine;
+import erogenousbeef.bigreactors.common.multiblock.PartTier;
+import erogenousbeef.bigreactors.common.multiblock.block.BlockMultiblockGlass;
+import erogenousbeef.bigreactors.common.multiblock.block.BlockTieredPart;
+import erogenousbeef.bigreactors.common.multiblock.tileentity.TileEntityReactorFuelRod;
+import erogenousbeef.bigreactors.init.BrBlocks;
+import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.client.renderer.block.statemap.IStateMapper;
+import net.minecraft.client.renderer.block.statemap.StateMap;
 import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.FMLClientHandler;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -43,26 +56,15 @@ public class ClientProxy extends CommonProxy {
 	public BlockBR register(BlockBR block) {
 
 		super.register(block);
+		block.onPostClientRegister();
+		return block;
+	}
 
-		if (block instanceof ICustomModelsProvider) {
+	@Override
+	public BlockBRGenericFluid register(BlockBRGenericFluid block) {
 
-			ICustomModelsProvider provider = (ICustomModelsProvider)block;
-			ResourceLocation location = provider.getCustomResourceLocation();
-			List<Pair<Integer, String>> mappings = ((ICustomModelsProvider)block).getMetadataToModelMappings();
-
-			if (null == location)
-				location = block.getRegistryName();
-
-			for (Pair<Integer, String> mapping : mappings) {
-
-				ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block),
-						mapping.getLeft().intValue(),
-						new ModelResourceLocation(location, mapping.getRight()));
-			}
-		} else
-			ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0,
-					new ModelResourceLocation(block.getRegistryName(), "inventory"));
-
+		super.register(block);
+		block.onPostClientRegister();
 		return block;
 	}
 
@@ -70,46 +72,33 @@ public class ClientProxy extends CommonProxy {
 	public ItemBase register(ItemBase item) {
 
 		super.register(item);
-
-		if (item instanceof ICustomModelsProvider) {
-
-			ICustomModelsProvider provider = (ICustomModelsProvider)item;
-			ResourceLocation location = provider.getCustomResourceLocation();
-			List<Pair<Integer, String>> mappings = ((ICustomModelsProvider)item).getMetadataToModelMappings();
-
-			if (null == location) {
-
-				location = item.getRegistryName();
-				location = new ResourceLocation(location.getResourceDomain(), "items/" + location.getResourcePath());
-			}
-
-			for (Pair<Integer, String> mapping : mappings) {
-
-				ModelLoader.setCustomModelResourceLocation(item,
-						mapping.getLeft().intValue(),
-						new ModelResourceLocation(location, mapping.getRight()));
-			}
-		}
-
+		item.onPostClientRegister();
 		return item;
 	}
 
+	/*
 	@Override
-	public void preInit() {}
+	public void preInit() {
+
+		super.preInit();
+	}*/
 
 	@Override
-	public void init()
-	{
+	public void init() {
+
 		super.init();
 
 		MinecraftForge.EVENT_BUS.register(new MultiblockClientTickHandler());
 		MinecraftForge.EVENT_BUS.register(new BRRenderTickHandler());
 
+		// register TESRs
+		this.registerTESRs();
+
 		// TODO Commented temporarily to allow this thing to compile...
 		/*
-		BlockFuelRod.renderId = RenderingRegistry.getNextAvailableRenderId();
+		BlockReactorFuelRod.renderId = RenderingRegistry.getNextAvailableRenderId();
 		ISimpleBlockRenderingHandler fuelRodISBRH = new SimpleRendererFuelRod();
-		RenderingRegistry.registerBlockHandler(BlockFuelRod.renderId, fuelRodISBRH);
+		RenderingRegistry.registerBlockHandler(BlockReactorFuelRod.renderId, fuelRodISBRH);
 		
 		BlockTurbineRotorPart.renderId = RenderingRegistry.getNextAvailableRenderId();
 		ISimpleBlockRenderingHandler rotorISBRH = new RotorSimpleRenderer();
@@ -142,5 +131,11 @@ public class ClientProxy extends CommonProxy {
 				}
 			}
 		}
+	}
+
+	private void registerTESRs() {
+
+		// reactor fuel rods
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityReactorFuelRod.class, new RendererReactorFuelRod());
 	}
 }
