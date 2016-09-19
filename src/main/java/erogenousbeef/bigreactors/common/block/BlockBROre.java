07a6395b476f1805f19747e42b0e3e2288dd8b35
ZeroNoRyouki <zeronoryouki@gmail.com>
2016-09-19 21:36:50 +0200

    Implemented getPickBlock(). Close #19

--- src/main/java/erogenousbeef/bigreactors/common/block/BlockBROre.java
+++ src/main/java/erogenousbeef/bigreactors/common/block/BlockBROre.java
@@ -12,10 +12,14 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.fml.relauncher.Side;
@@ -136,6 +140,17 @@ public class BlockBROre extends BlockBR {
 		MineralType mineral = type.getMineralDropped();
 
 		return null == mineral ? super.damageDropped(state) : mineral.toMeta();
+	}
+
+	/**
+	 * Called when a user uses the creative pick block button on this block
+	 *
+	 * @param target The full target the player is looking at
+	 * @return A ItemStack to add to the player's inventory, Null if nothing should be added.
+	 */
+	@Override
+	public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player) {
+		return this.createItemStack(state.getValue(Properties.ORE), 1);
 	}
 
 	public ItemStack createItemStack(OreType type, int amount) {
