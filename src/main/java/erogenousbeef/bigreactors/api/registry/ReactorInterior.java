2bec1d22bffa36b56657ad0fb5335bac2ce1cd4f
ZeroNoRyouki <zeronoryouki@gmail.com>
2016-11-12 19:08:06 +0100

    API: added methods to remove blocks and fluids from the reactor's moderators and turbine coils registries. Re-worked internal code a bit

--- src/main/java/erogenousbeef/bigreactors/api/registry/ReactorInterior.java
+++ src/main/java/erogenousbeef/bigreactors/api/registry/ReactorInterior.java
@@ -1,60 +1,86 @@
 package erogenousbeef.bigreactors.api.registry;
 
 import erogenousbeef.bigreactors.api.data.ReactorInteriorData;
-import erogenousbeef.bigreactors.common.BRLog;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.FMLLog;
 import net.minecraftforge.oredict.OreDictionary;
 
+import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ReactorInterior {
-	private static Map<String, ReactorInteriorData> _reactorModeratorBlocks = new HashMap<String, ReactorInteriorData>();
-	private static Map<String, ReactorInteriorData> _reactorModeratorFluids = new HashMap<String, ReactorInteriorData>();
 
 	/**
-	 * @param absorption	How much radiation this material absorbs and converts to heat. 0.0 = none, 1.0 = all.
+	 * Register a block as a radiation moderator for the Reactor
+	 * If the block is already registered, the provided values will replace the existing ones
+	 *
+	 * @param oreDictName The ore-dictionary name of the block
+	 * @param absorption How much radiation this material absorbs and converts to heat. 0.0 = none, 1.0 = all.
 	 * @param heatEfficiency How efficiently radiation is converted to heat. 0 = no heat, 1 = all heat.
-	 * @param moderation	How well this material moderates radiation. This is a divisor; should not be below 1.
+	 * @param moderation How well this material moderates radiation. This is a divisor; should not be below 1.
+	 * @param heatConductivity How well this material conducts heat, in RF/t/m2.
 	 */
 	public static void registerBlock(String oreDictName, float absorption, float heatEfficiency, float moderation, float heatConductivity) {
-		if(_reactorModeratorBlocks.containsKey(oreDictName)) {
-			BRLog.warning("Overriding existing radiation moderator block data for oredict name <%s>", oreDictName);
-			ReactorInteriorData data = _reactorModeratorBlocks.get(oreDictName);
-			data.absorption = absorption;
-			data.heatEfficiency = heatEfficiency;
-			data.moderation = moderation;
-		}
-		else {
-			_reactorModeratorBlocks.put(oreDictName, new ReactorInteriorData(absorption, heatEfficiency, moderation, heatConductivity));
-		}
+		register(true, oreDictName, absorption, heatEfficiency, moderation, heatConductivity);
 	}
 
 	/**
-	 * @param absorption	How much radiation this material absorbs and converts to heat. 0.0 = none, 1.0 = all.
+	 * Deregister a previously registered radiation moderator block for the Reactor
+	 *
+	 * @param oreDictName The ore-dictionary name of the block
+	 * @return The previously registered data for the block or null if the block was not registered
+	 */
+	@Nullable
+	public static ReactorInteriorData deregisterBlock(String oreDictName) {
+		return deregister(true, oreDictName);
+	}
+
+	/**
+	 * Register a fluid as a radiation moderator for the Reactor
+	 * If the fluid is already registered, the provided values will replace the existing ones
+	 *
+	 * @param fluidName The ore-dictionary name of the block
+	 * @param absorption How much radiation this material absorbs and converts to heat. 0.0 = none, 1.0 = all.
 	 * @param heatEfficiency How efficiently radiation is converted to heat. 0 = no heat, 1 = all heat.
-	 * @param moderation	How well this material moderates radiation. This is a divisor; should not be below 1.
+	 * @param moderation How well this material moderates radiation. This is a divisor; should not be below 1.
+	 * @param heatConductivity How well this material conducts heat, in RF/t/m2.
 	 */
 	public static void registerFluid(String fluidName, float absorption, float heatEfficiency, float moderation, float heatConductivity) {
-		if(_reactorModeratorFluids.containsKey(fluidName)) {
-			BRLog.warning("Overriding existing radiation moderator fluid data for fluid name <%s>", fluidName);
-			ReactorInteriorData data = _reactorModeratorFluids.get(fluidName);
-			data.absorption = absorption;
-			data.heatEfficiency = heatEfficiency;
-			data.moderation = moderation;
-		}
-		else {
-			_reactorModeratorFluids.put(fluidName, new ReactorInteriorData(absorption, heatEfficiency, moderation, heatConductivity));
-		}
+		register(false, fluidName, absorption, heatEfficiency, moderation, heatConductivity);
 	}
-	
+
+	/**
+	 * Deregister a previously registered radiation moderator fluid for the Reactor
+	 *
+	 * @param fluidName The registry name of the fluid
+	 * @return The previously registered data for the fluid or null if the fluid was not registered
+	 */
+	@Nullable
+	public static ReactorInteriorData deregisterFluid(String fluidName) {
+		return deregister(false, fluidName);
+	}
+
+	/**
+	 * Retrieve the radiation moderation data for the given block
+	 *
+	 * @param oreDictName The ore-dictionary name of the block
+	 * @return The radiation moderation data registered for the block or null if the block was not registered
+	 */
+	@Nullable
 	public static ReactorInteriorData getBlockData(String oreDictName) {
 		return _reactorModeratorBlocks.get(oreDictName);
 	}
 
+	/**
+	 * Retrieve the radiation moderation data for a block compatible to the given one
+	 *
+	 * @param stack an ItemStack containing the item block to check for
+	 * @return The radiation moderation data registered for the block or null if the block was not registered
+	 */	@Nullable
 	public static ReactorInteriorData getBlockData(ItemStack stack) {
 
-		int[] ids = null != stack ? OreDictionary.getOreIDs(stack) : null;
+		final int[] ids = null != stack ? OreDictionary.getOreIDs(stack) : null;
 		int len;
 
 		if (null == ids || 0 == (len = ids.length))
@@ -67,13 +93,54 @@ public class ReactorInterior {
 			name = OreDictionary.getOreName(ids[i]);
 
 			if (_reactorModeratorBlocks.containsKey(name))
-				return  _reactorModeratorBlocks.get(name);
+				return _reactorModeratorBlocks.get(name);
 		}
 
 		return null;
 	}
 
+	@Nullable
 	public static ReactorInteriorData getFluidData(String fluidName) {
 		return _reactorModeratorFluids.get(fluidName);
-	}	
+	}
+
+
+
+	private static void register(final boolean isBlock, final String name, final float absorption,
+								 final float heatEfficiency, final float moderation, final float heatConductivity) {
+
+		final Map<String, ReactorInteriorData> registry = isBlock ? _reactorModeratorBlocks : _reactorModeratorFluids;
+
+		if (null == name || name.isEmpty()) {
+
+			FMLLog.warning("Blocked an attempt to register a new radiation moderator with an empty name");
+			return;
+		}
+
+		if (registry.containsKey(name)) {
+
+			FMLLog.warning("Overriding existing radiation moderator data for <%s>", name);
+
+			final ReactorInteriorData data = registry.get(name);
+
+			data.absorption = absorption;
+			data.heatEfficiency = heatEfficiency;
+			data.moderation = moderation;
+
+		} else {
+
+			registry.put(name, new ReactorInteriorData(absorption, heatEfficiency, moderation, heatConductivity));
+		}
+	}
+
+	@Nullable
+	private static ReactorInteriorData deregister(final boolean isBlock, final String name) {
+
+		final Map<String, ReactorInteriorData> registry = isBlock ? _reactorModeratorBlocks : _reactorModeratorFluids;
+
+		return null != name && !name.isEmpty() ? registry.remove(name) : null;
+	}
+
+	private static Map<String, ReactorInteriorData> _reactorModeratorBlocks = new HashMap<String, ReactorInteriorData>();
+	private static Map<String, ReactorInteriorData> _reactorModeratorFluids = new HashMap<String, ReactorInteriorData>();
 }
--- src/main/java/erogenousbeef/bigreactors/api/registry/TurbineCoil.java
+++ src/main/java/erogenousbeef/bigreactors/api/registry/TurbineCoil.java
@@ -2,7 +2,9 @@ package erogenousbeef.bigreactors.api.registry;
 
 import erogenousbeef.bigreactors.api.data.CoilPartData;
 import erogenousbeef.bigreactors.common.BRLog;
+import net.minecraftforge.fml.common.FMLLog;
 
+import javax.annotation.Nullable;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,22 +13,53 @@ public class TurbineCoil {
 
 	/**
 	 * Register a block as permissible in a turbine's inductor coil.
+	 *
 	 * @param oreDictName Name of the block, as registered in the ore dictionary
 	 * @param efficiency  Efficiency of the block. 1.0 == iron, 2.0 == gold, etc.
 	 * @param bonus		  Energy bonus of the block, if any. Normally 1.0. This is an exponential term and should only be used for EXTREMELY rare blocks!
 	 */
 	public static void registerBlock(String oreDictName, float efficiency, float bonus, float extractionRate) {
-		if(_blocks.containsKey(oreDictName)) {
-			CoilPartData data = _blocks.get(oreDictName);
-			BRLog.warning("Overriding existing coil part data for oredict name <%s>, original values: eff %.2f / bonus %.2f, new values: eff %.2f / bonus %.2f", oreDictName, data.efficiency, data.bonus, efficiency, bonus); 
+
+		if (null == oreDictName || oreDictName.isEmpty()) {
+
+			FMLLog.warning("Blocked an attempt to register a new block in the turbine's inductor coil");
+			return;
+		}
+
+		if (_blocks.containsKey(oreDictName)) {
+
+			final CoilPartData data = _blocks.get(oreDictName);
+
+			FMLLog.warning("Overriding existing coil part data for oredict name <%s>, original values: eff %.2f / bonus %.2f, new values: eff %.2f / bonus %.2f",
+					oreDictName, data.efficiency, data.bonus, efficiency, bonus);
+
 			data.efficiency = efficiency;
 			data.bonus = bonus;
-		}
-		else {
+
+		} else {
+
 			_blocks.put(oreDictName, new CoilPartData(efficiency, bonus, extractionRate));
 		}
 	}
 
+	/**
+	 * Deregister a previously registered block from the turbine's inductor coil.
+	 *
+	 * @param oreDictName Name of the block, as registered in the ore dictionary
+	 * @return The previously registered data for the block or null if the block was not registered
+	 */
+	@Nullable
+	public static CoilPartData deregisterBlock(String oreDictName) {
+		return null != oreDictName && !oreDictName.isEmpty() ? _blocks.remove(oreDictName) : null;
+	}
+
+	/**
+	 * Retrieve the coil data for the given block
+	 *
+	 * @param oreDictName The ore-dictionary name of the block
+	 * @return The coil data registered for the block or null if the block was not registered
+	 */
+	@Nullable
 	public static CoilPartData getBlockData(String oreDictName) {
 		return _blocks.get(oreDictName);
 	}
