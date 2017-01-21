70eacd1c80267dd6d70cb49272ce64d54201d0d3
ZeroNoRyouki <zeronoryouki@gmail.com>
2017-01-21 13:35:50 +0100

    Creative Tab are not displayed that ofthen of no need to cache the icon

--- src/main/java/erogenousbeef/bigreactors/common/CreativeTabBR.java
+++ src/main/java/erogenousbeef/bigreactors/common/CreativeTabBR.java
@@ -1,18 +1,20 @@
 package erogenousbeef.bigreactors.common;
 
+import erogenousbeef.bigreactors.common.block.OreType;
 import erogenousbeef.bigreactors.init.BrBlocks;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class CreativeTabBR extends CreativeTabs {
 
-	public CreativeTabBR(String label)
-	{
+	public CreativeTabBR(String label) {
 		super(label);
 	}
 
-	public Item getTabIconItem()
-	{
-		return Item.getItemFromBlock(BrBlocks.brOre);
+	@SideOnly(Side.CLIENT)
+	public ItemStack getTabIconItem() {
+		return BrBlocks.brOre.createItemStack(OreType.Yellorite, 1);
 	}
 }
