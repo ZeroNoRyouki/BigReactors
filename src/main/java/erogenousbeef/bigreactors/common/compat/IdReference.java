b8e416079bc65e662b56dfb1dd716c0b5b4db260
ZeroNoRyouki <zeronoryouki@gmail.com>
2017-05-04 18:27:35 +0200

    Changed mod-compat mod Ids to their 1.10.x versions

--- src/main/java/erogenousbeef/bigreactors/common/compat/IdReference.java
+++ src/main/java/erogenousbeef/bigreactors/common/compat/IdReference.java
@@ -2,11 +2,11 @@ package erogenousbeef.bigreactors.common.compat;
 
 public final class IdReference {
 
-    public static final String MODID_COMPUTERCRAFT = "computercraft";
-    public static final String MODID_OPENCOMPUTERS = "opencomputers";
-    public static final String MODID_MEKANISM = "mekanism";
+    public static final String MODID_COMPUTERCRAFT = "ComputerCraft";
+    public static final String MODID_OPENCOMPUTERS = "OpenComputers";
+    public static final String MODID_MEKANISM = "Mekanism";
     public static final String MODID_THERMALEXPANSION = "thermalexpansion";
-    public static final String MODID_MINEFACTORYRELOADED = "minefactoryreloaded";
+    public static final String MODID_MINEFACTORYRELOADED = "MinefactoryReloaded";
     public static final String MODID_APPLIEDENERGISTICS2 = "appliedenergistics2";
 
     private IdReference() {
