db293a6386c4e01c43d8e3cdd233379ce6e7c967
ZeroNoRyouki <zeronoryouki@gmail.com>
2017-06-25 21:16:10 +0200

    Starting to re-integrate MFR

--- src/main/java/erogenousbeef/bigreactors/common/multiblock/block/BlockReactorRedNetPort.java
+++ src/main/java/erogenousbeef/bigreactors/common/multiblock/block/BlockReactorRedNetPort.java
@@ -1,19 +1,29 @@
-//TODO This is just a placeholder while we wait for MineFactory Reloaded to be available again
-
 package erogenousbeef.bigreactors.common.multiblock.block;
 
+import erogenousbeef.bigreactors.common.compat.CompatManager;
+import erogenousbeef.bigreactors.common.compat.IdReference;
+import erogenousbeef.bigreactors.common.multiblock.PartTier;
 import erogenousbeef.bigreactors.common.multiblock.PartType;
-import erogenousbeef.bigreactors.common.multiblock.tileentity.TileEntityReactorPart;
+import erogenousbeef.bigreactors.common.multiblock.tileentity.TileEntityReactorRedNetPort;
+import erogenousbeef.bigreactors.init.BrBlocks;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.Optional;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import powercrystals.minefactoryreloaded.api.rednet.IRedNetOmniNode;
+import powercrystals.minefactoryreloaded.api.rednet.connectivity.RedNetConnectionType;
 
-/*
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 @Optional.InterfaceList({
-	@Optional.Interface(iface = "powercrystals.minefactoryreloaded.api.rednet.IRedNetOmniNode", modid = "MineFactoryReloaded")
+	@Optional.Interface(iface = "powercrystals.minefactoryreloaded.api.rednet.IRedNetOmniNode", modid = IdReference.MODID_MINEFACTORYRELOADED)
 })
-*/
-public class BlockReactorRedNetPort extends BlockMultiblockDevice /*implements IRedNetOmniNode*/ {
+public class BlockReactorRedNetPort extends BlockMultiblockDevice implements IRedNetOmniNode {
 
     public BlockReactorRedNetPort(String blockName) {
         super(PartType.ReactorRednetPort, blockName);
@@ -21,8 +31,22 @@ public class BlockReactorRedNetPort extends BlockMultiblockDevice /*implements I
 
     @Override
     public TileEntity createTileEntity(World world, IBlockState state) {
-        return new TileEntityReactorPart();
-        //return new TileEntityReactorRedNetPort();
+        return new TileEntityReactorRedNetPort();
+    }
+
+    @Override
+    public void registerRecipes() {
+
+        if (!CompatManager.isModLoaded(IdReference.MODID_MINEFACTORYRELOADED))
+            return;
+
+        if (PartTier.REACTOR_TIERS.contains(PartTier.Legacy))
+            GameRegistry.addRecipe(new ShapedOreRecipe(this.createItemStack(PartTier.Legacy, 1), "CRC", "RGR", "CRC",
+                    'C', 'C', BrBlocks.reactorCasing.createItemStack(PartTier.Legacy, 1), 'R', "cableRedNet", 'G', "ingotGold"));
+
+        if (PartTier.REACTOR_TIERS.contains(PartTier.Basic))
+            GameRegistry.addRecipe(new ShapedOreRecipe(this.createItemStack(PartTier.Basic, 1), "CRC", "RGR", "CRC",
+                    'C', 'C', BrBlocks.reactorCasing.createItemStack(PartTier.Basic, 1), 'R', "cableRedNet", 'G', "ingotGold"));
     }
 
     /* TODO but back recipe when MineFactory Reloaded is back
@@ -42,67 +66,76 @@ public class BlockReactorRedNetPort extends BlockMultiblockDevice /*implements I
             return _redNetPortConfigIcons[circuitType.ordinal() - 1];
         }
     }
+    */
 
     // IConnectableRedNet
-    @Optional.Method(modid = "MineFactoryReloaded")
-    @Override
-    public RedNetConnectionType getConnectionType(World world, int x, int y,
-                                                  int z, ForgeDirection side) {
-        TileEntity te = world.getTileEntity(x, y, z);
-        if(te instanceof TileEntityReactorRedNetPort) {
-            return RedNetConnectionType.CableAll;
-        }
 
-        return RedNetConnectionType.None;
+    @Override
+    @Optional.Method(modid = IdReference.MODID_MINEFACTORYRELOADED)
+    public RedNetConnectionType getConnectionType(World world, BlockPos position, EnumFacing enumFacing) {
+
+        final TileEntityReactorRedNetPort te = this.getPortTile(world, position);
+
+        return null != te ? RedNetConnectionType.CableAll : RedNetConnectionType.None;
     }
 
-    @Optional.Method(modid = "MineFactoryReloaded")
     @Override
-    public int[] getOutputValues(World world, int x, int y, int z,
-                                 ForgeDirection side) {
-        TileEntity te = world.getTileEntity(x, y, z);
-        if(te instanceof TileEntityReactorRedNetPort) {
-            return ((TileEntityReactorRedNetPort)te).getOutputValues();
-        }
-        else {
+    @Optional.Method(modid = IdReference.MODID_MINEFACTORYRELOADED)
+    public int[] getOutputValues(World world, BlockPos position, EnumFacing enumFacing) {
+
+        final TileEntityReactorRedNetPort te = this.getPortTile(world, position);
+
+        if (null != te) {
+
+            return te.getOutputValues();
+
+        } else {
+
             int[] values = new int[16];
-            for(int i = 0; i < 16; i++) {
+
+            for(int i = 0; i < 16; i++)
                 values[i] = 0;
-            }
+
             return values;
         }
     }
 
     // Never used. we're always in "all" mode.
-    @Optional.Method(modid = "MineFactoryReloaded")
     @Override
-    public int getOutputValue(World world, int x, int y, int z,
-                              ForgeDirection side, int subnet) {
-        TileEntity te = world.getTileEntity(x, y, z);
-        if(te instanceof TileEntityReactorRedNetPort) {
-            return ((TileEntityReactorRedNetPort)te).getValueForChannel(subnet);
-        }
+    @Optional.Method(modid = IdReference.MODID_MINEFACTORYRELOADED)
+    public int getOutputValue(World world, BlockPos position, EnumFacing enumFacing, int subnet) {
+
+        final TileEntityReactorRedNetPort te = this.getPortTile(world, position);
+
+        if (null != te)
+            te.getValueForChannel(subnet);
+
         return 0;
     }
 
-    @Optional.Method(modid = "MineFactoryReloaded")
     @Override
-    public void onInputsChanged(World world, int x, int y, int z,
-                                ForgeDirection side, int[] inputValues) {
-        TileEntity te = world.getTileEntity(x, y, z);
-        if(te instanceof TileEntityReactorRedNetPort) {
-            ((TileEntityReactorRedNetPort)te).onInputValuesChanged(inputValues);
-        }
+    @Optional.Method(modid = IdReference.MODID_MINEFACTORYRELOADED)
+    public void onInputsChanged(World world, BlockPos position, EnumFacing enumFacing, int[] inputValues) {
+
+        final TileEntityReactorRedNetPort te = this.getPortTile(world, position);
+
+        if (null != te)
+            te.onInputValuesChanged(inputValues);
     }
 
     // Never used, we're always in "all" mode.
-    @Optional.Method(modid = "MineFactoryReloaded")
     @Override
-    public void onInputChanged(World world, int x, int y, int z,
-                               ForgeDirection side, int inputValue) {
-        return;
+    @Optional.Method(modid = IdReference.MODID_MINEFACTORYRELOADED)
+    public void onInputChanged(World world, BlockPos position, EnumFacing enumFacing, int inputValue) {
     }
 
-    private IIcon[] _redNetPortConfigIcons = new IIcon[TileEntityReactorRedNetPort.CircuitType.values().length - 1];
-    */
+    @Nullable
+    private TileEntityReactorRedNetPort getPortTile(@Nonnull final World world, @Nonnull final BlockPos position) {
+
+        TileEntity te = world.getTileEntity(position);
+
+        return te instanceof TileEntityReactorRedNetPort ? (TileEntityReactorRedNetPort)te : null;
+    }
+
+    //private IIcon[] _redNetPortConfigIcons = new IIcon[TileEntityReactorRedNetPort.CircuitType.values().length - 1];
 }
